059a4ad9a353870b64aa0e12f87e5600c948e76a
Pho <pho.dmc5@gmail.com>
2025-02-10 18:10:03 +0000

    feat: solve leapYears and tempConversion problems

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  if (fahrenheit == 32) return 0;
+
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  return Number.parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  if (celsius == 0) return 32;
+
+  let fahrenheit = celsius * (9 / 5) + 32;
+  return Number.parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
